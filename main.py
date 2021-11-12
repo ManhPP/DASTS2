ea03b1c31499d0eea92359e2692074181a8f628f
manhpp <phumanh1998@gmail.com>
2021-11-12 15:49:17 +0700

    fc

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+import glob
+import os
+from datetime import datetime
+
+from omegaconf import OmegaConf
+
+from src.gurobi_ip import solve_by_gurobi
+from src.util import load_input
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='DASTS2')
+
+    parser.add_argument('--config', type=str, default="config.yml",
+                        help='path of config file')
+    args = parser.parse_args()
+    config = OmegaConf.load(args.config)
+    config.result_folder = os.path.join(config.result_folder, datetime.now().strftime("%m%d%Y%H%M%S"))
+    for data_path in config.data_path.split(","):
+        paths = glob.glob(data_path)
+        print(paths)
+        for data_set in paths:
+            print(data_set)
+            try:
+                inp = load_input(config, data_set)
+
+                solve_by_gurobi(config, inp)
+
+            except Exception as e:
+                print("Error: ", e)
