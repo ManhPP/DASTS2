7cd91d12826f7438e4240f92ebff7c352b47efeb
manhpp <phumanh1998@gmail.com>
2022-05-16 00:25:05 +0700

    update

--- src/ts/init_solution.py
+++ src/ts/init_solution.py
@@ -1,27 +1,86 @@
-import numpy as np
-from sklearn.cluster import KMeans
-
-
-def init(inp, config):
+def init_1(inp, config, reverse=False):
     solution = []
-    coordinates_matrix = np.loadtxt(config.data_path, skiprows=2)[:, :2]
+    num_drone = config.params.num_drone
+    num_staff = config.params.num_staff
+    for i in range(num_drone + num_staff):
+        solution.append([])
 
-    C1 = inp["C1"]
-    X = []
-    for i in C1:
-        X.append(coordinates_matrix[i])
+    sorted_neighborhood = {}
+    for cus in range(inp['num_cus'] + 1):
+        tmp = {}
+        for neighbor in range(inp['num_cus'] + 1):
+            if cus != neighbor:
+                tmp[neighbor] = inp['tau'][cus, neighbor]
 
-    num_cluster = min(config.params.num_staff, max(2, config.params.num_staff / 2))
-    if 2 <= num_cluster <= len(C1):
-        kmeans = KMeans(n_clusters=num_cluster, random_state=0).fit(np.array(X))
-        clusters = kmeans.labels_
-    else:
-        clusters = [0] * len(C1)
+        tmp = sorted(tmp.items(), key=lambda e: e[1], reverse=reverse)
+        sorted_neighborhood[cus] = [i for i, _ in tmp]
 
-    cus2lab = {}
-    for i in range(1, inp['num_cus'] + 1):
-        cus2lab[i] = inp['tau'][0, i]
+    travel_time = [0] * (num_drone + num_staff)
 
-    sorted_cus = [i for i, _ in sorted(cus2lab.items(), key=lambda x: x[1])]
+    i = 0
+    C1 = inp['C1']
+    visited_cus = []
+
+    while len(visited_cus) < inp['num_cus']:
+        time_dict = inp['tau']
+        if i < num_drone and len(solution[i]) == 0:
+            solution[i].append([])
+
+        if i < num_drone:
+            time_dict = inp['tau_a']
+            cur_trip = solution[i][-1]
+        else:
+            cur_trip = solution[i]
+
+        if len(cur_trip) == 0:
+            last_cus = 0
+            first_cus = 0
+        else:
+            last_cus = cur_trip[-1]
+            first_cus = cur_trip[0]
+
+        next_cus = None
+        for nc in sorted_neighborhood[last_cus]:
+            if nc == 0:
+                continue
+            if nc not in visited_cus:
+                if i >= num_drone:
+                    next_cus = nc
+                    break
+                if i < num_drone and i not in C1:
+                    next_cus = nc
+                    break
+
+        if next_cus is None:
+            continue
+
+        if i < num_drone:
+            if travel_time[i] + time_dict[last_cus, next_cus] + time_dict[next_cus, 0] > config.params.L_d or \
+                    travel_time[i] + time_dict[last_cus, next_cus] + time_dict[next_cus, 0] - time_dict[0, first_cus] \
+                    > config.params.L_w:
+                solution[i].append([next_cus])
+                travel_time[i] = time_dict[0, next_cus]
+            else:
+                cur_trip.append(next_cus)
+                travel_time[i] = travel_time[i] + time_dict[last_cus, next_cus]
+            visited_cus.append(next_cus)
+        else:
+            if travel_time[i] + time_dict[last_cus, next_cus] + time_dict[next_cus, 0] - time_dict[0, first_cus] \
+                    <= config.params.L_w:
+                cur_trip.append(next_cus)
+                travel_time[i] = travel_time[i] + time_dict[last_cus, next_cus]
+                visited_cus.append(next_cus)
+        i += 1
+        i %= num_drone + num_staff
+
+    return solution
+
+
+def init_2(inp, config):
+    solution = []
+    num_drone = config.params.num_drone
+    num_staff = config.params.num_staff
+    for i in range(num_drone + num_staff):
+        solution.append([])
 
     return solution
